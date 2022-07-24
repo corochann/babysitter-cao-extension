f4a61d2802fec82f2f157ef907701b3031e45508
corochann <acc1ssnn9terias@gmail.com>
2022-07-24 14:19:48 +0900

    update

--- content.js
+++ content.js
@@ -1,27 +1,39 @@
-const storageItemNames = [
-    'start-date', 'start-hours', 'start-minutes',
-    'end-date', 'end-hours', 'end-minutes',
-    'child-name', 'child-birthday', 'babysitter-name',
-    'place'
-]
+function fillForm() {
+    const storageItemNames = [
+        'start-date', 'start-hours', 'start-minutes',
+        'end-date', 'end-hours', 'end-minutes',
+        'child-name', 'child-birthday', 'babysitter-name',
+        'place'
+    ]
 
-// すでに保存されている情報があればそれを設定する処理
-chrome.storage.sync.get(storageItemNames, items => {
-    // すでに保存されている情報があればそれを設定する処理
-    document.getElementById("atms-ticket-form-start").value = items["start-date"];
-    document.getElementById("atms-ticket-form-start-hours").value = items["start-hours"];  // start
-    document.getElementById("atms-ticket-form-start-minutes").value = items["start-minutes"];  // start
-    document.getElementById("atms-ticket-form-end").value = items["end-date"];  // end
-    document.getElementById("atms-ticket-form-end-hours").value = items["end-hours"];
-    document.getElementById("atms-ticket-form-end-minutes").value = items["end-minutes"];
+    // Set the word, which is stored in storage.
+    chrome.storage.sync.get(storageItemNames, items => {
+        document.getElementById("atms-ticket-form-start").value = items["start-date"];
+        document.getElementById("atms-ticket-form-start-hours").value = items["start-hours"];  // start
+        document.getElementById("atms-ticket-form-start-minutes").value = items["start-minutes"];  // start
+        document.getElementById("atms-ticket-form-end").value = items["end-date"];  // end
+        document.getElementById("atms-ticket-form-end-hours").value = items["end-hours"];
+        document.getElementById("atms-ticket-form-end-minutes").value = items["end-minutes"];
 
-    document.getElementById("atms-form-free-item0").value = items["child-name"];
-    document.getElementById("atms-form-free-item3").value = items["place"];
+        document.getElementById("atms-form-free-item0").value = items["child-name"];
+        document.getElementById("atms-form-free-item3").value = items["place"];
 
-    var elements = document.querySelectorAll("#atms-form-free-item2");
-    elements[0].value = items["child-birthday"];  // Birthday
-    elements[1].value = items["babysitter-name"];    // Babysitter name
+        var elements = document.querySelectorAll("#atms-form-free-item2");
+        elements[0].value = items["child-birthday"];  // Birthday
+        elements[1].value = items["babysitter-name"];    // Babysitter name
+    });
+
+    // This does not work. It won't enable "next" button...
+    //document.getElementById("policy-confirm").checked = true;
+}
+
+// Apply when window is loaded.
+//fillForm();
+
+chrome.runtime.onMessage.addListener(message => {
+    console.log(`message: ${message}`)
+    if (message.type === 'SUBMIT') {
+        fillForm();
+    }
+    return true;
 });
-
-// This does not work. It won't enable "next" button...
-//document.getElementById("policy-confirm").checked = true;
--- popup.js
+++ popup.js
@@ -2,37 +2,44 @@
 // https://qiita.com/RyBB/items/3f343252b0397e93050e
 window.onload = ()  => {
     const messageEl = document.getElementById('message');
-    const submitBtnEl = document.getElementById('submit');
+    const saveBtnEl = document.getElementById('submit');
     // --- storage element ---
     const storageItemNames = [
         'start-date', 'start-hours', 'start-minutes',
         'end-date', 'end-hours', 'end-minutes',
         'child-name', 'child-birthday', 'babysitter-name',
         'place'
-    ]
+    ];
 
     // すでに保存されている情報があればそれを設定する処理
     chrome.storage.sync.get(storageItemNames, items => {
         // すでに保存されている情報があればそれを設定する処理
         for (let i = 0; i < storageItemNames.length; i++) {
-            let storageItemName = storageItemNames[i]
-            let storageItem = items[storageItemName]
-            console.log(storageItemName + ' -> ' + storageItem)
+            let storageItemName = storageItemNames[i];
+            let storageItem = items[storageItemName];
+            console.log(storageItemName + ' -> ' + storageItem);
             document.getElementById(storageItemName).value = storageItem;
         }
     });
 
     // Option画面で保存されたときの処理
-    submitBtnEl.onclick = () => {
-        let savedDict = {}
+    saveBtnEl.onclick = () => {
+        let savedDict = {};
         for (let i = 0; i < storageItemNames.length; i++) {
-            let storageItemName = storageItemNames[i]
-            savedDict[storageItemName] = document.getElementById(storageItemName).value
+            let storageItemName = storageItemNames[i];
+            savedDict[storageItemName] = document.getElementById(storageItemName).value;
         }
-        console.log(savedDict)
+        console.log(`savedDict: ${savedDict}`);
         chrome.storage.sync.set(savedDict, () => {
             messageEl.textContent = 'Saved';
             setTimeout(() => messageEl.textContent = '', 750);
+        });
+
+        console.log('tabs SUBMIT');
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: 'SUBMIT',
+            });
         });
     }
 };
